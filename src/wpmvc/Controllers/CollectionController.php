568b712b12f91b5969ef309d9b56ce91f42c9694
Jen Wachter <jwachter@jhu.edu>
2013-03-10 13:39:42 -0400

    Added docs to controllers.

--- src/wpmvc/Controllers/CollectionController.php
+++ src/wpmvc/Controllers/CollectionController.php
@@ -6,6 +6,13 @@ class CollectionController extends Base
 {
 	protected $modelName = "Collection";
 
+	/**
+	 * Runs the parent constructor, gets the data
+	 * from the model and renders the view.
+	 * @param string $template Absolute location of template.
+	 * @param array  $options  Key/value query options
+	 * @return null
+	 */
 	public function __construct($template, $options = array())
 	{
 		parent::__construct($template, $options);
