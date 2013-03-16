e17da5ab54d99a59483dc0b1c3a6c3b46ca8e2fe
Jen Wachter <jwachter@jhu.edu>
2013-03-16 11:56:57 -0400

    Created pager object

---
+++ src/wpmvc/Helpers/Pager.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace wpmvc\Helpers;
+
+class Pager
+{
+	protected $id;
+
+	protected $totalPages;
+
+	protected $currentPage;
+
+	/**
+	 * [__construct description]
+	 * @param object $query Query helper object
+	 * @param string $type  "collection" or "post"
+	 */
+	public function __construct($args)
+	{
+		foreach ($args as $k => $v) {
+			if (property_exists($this, $k)) {
+				$this->$k = $v;
+			}
+		}
+
+		if (!is_null($this->id)) {
+			global $post;
+			$post = get_post($this->id);
+		}
+	}
+
+	public function paginate()
+	{
+		return array(
+			"currentPage" => $this->currentPage,
+			"nextPage" => $this->nextPage(),
+			"prevPage" => $this->prevPage()
+		);
+	}
+
+	protected function getPostNavArgs($post)
+	{
+		if (empty($post)) {
+			return null;
+		}
+		
+		$author = get_userdata($post->post_author);
+		$author = $author->data->user_nicename;
+
+		return array(
+			"id" => $post->ID,
+			"slug" => $post->post_name,
+			"pubDate" => strtotime($post->post_date_gmt),
+			"author" => $author,
+			"categories" => wp_get_post_terms($post->ID, "category"),
+			"tags" => wp_get_post_terms($post->ID)
+
+		);
+	}
+
+	protected function nextPage()
+	{
+		if (!is_null($this->id)) {
+			$nextPage = get_adjacent_post(false, "", false);
+			return $this->getPostNavArgs($nextPage);
+
+		} else if (!is_null($this->currentPage) && !is_null($this->totalPages)) {
+			return $this->currentPage < $this->totalPages ? $this->currentPage + 1 : null;
+		}
+
+		return null;
+	}
+
+	protected function prevPage()
+	{
+		if (!is_null($this->id)) {
+			$prevPage = get_adjacent_post();
+			return $this->getPostNavArgs($prevPage);
+
+		} else if (!is_null($this->currentPage) && !is_null($this->totalPages)) {
+			return $this->currentPage > 1 ? $this->currentPage - 1 : null;
+		}
+	}
+}
