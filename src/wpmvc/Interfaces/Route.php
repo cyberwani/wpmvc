0c39de326cdf2b893a2ad844d90ce197c797aa3a
Jen Wachter <jwachter@jhu.edu>
2013-06-08 14:18:50 -0400

    Added router exchange to wpmvc.

---
+++ src/wpmvc/Interfaces/Route.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace wpmvc\Interfaces;
+
+interface Route
+{
+	// Route methods
+	public function name($name);
+	public function condition($param, $regex);
+
+	// Middleware
+	public function before($callable);
+	public function after($callable);
+}
---
+++ src/wpmvc/Interfaces/Router.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace wpmvc\Interfaces;
+
+interface Router
+{
+	// Settings
+	public function setDebug($boolean);
+
+	// HTTP methods
+	public function get($pattern, $callable);
+	public function post($pattern, $callable);
+	public function put($pattern, $callable);
+	public function delete($pattern, $callable);
+	public function options($pattern, $callable);
+
+	// Router helpers
+	public function redirect($url);
+	public function abort($code, $message);
+
+	// Error reporting
+	public function error($callable);
+
+	// Run the router
+	public function run();
+}
