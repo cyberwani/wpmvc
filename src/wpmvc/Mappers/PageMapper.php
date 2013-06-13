1faf6dc190816788ad346e0984d6f651028c232f
Jen Wachter <jwachter@jhu.edu>
2013-06-13 12:31:03 -0400

    Added prepareOptions() back to page and post mapper.

--- src/wpmvc/Mappers/PageMapper.php
+++ src/wpmvc/Mappers/PageMapper.php
@@ -6,4 +6,21 @@ class PageMapper extends Base
 {
 	protected $object = "Page";
 	protected $collection = "PageCollection";
+
+	/**
+	 * Run any necessary alterations/additions
+	 * to the options and then set off to the model.
+	 * @param  array $options Key/value query options
+	 * @return null
+	 */
+	protected function prepareOptions($options)
+	{
+		$options["type"] = "page";
+		if (isset($options["id"])) {
+			$options["page_id"] = $options["id"];
+			unset($options["id"]);
+		}
+
+		parent::prepareOptions($options);
+	}
 }
--- src/wpmvc/Mappers/PostMapper.php
+++ src/wpmvc/Mappers/PostMapper.php
@@ -6,4 +6,20 @@ class PostMapper extends Base
 {
 	protected $object = "Post";
 	protected $collection = "PostCollection";
+
+	/**
+	 * Run any necessary alterations/additions
+	 * to the options and then set off to the model.
+	 * @param  array $options Key/value query options
+	 * @return null
+	 */
+	protected function prepareOptions($options)
+	{
+		if (isset($options["id"])) {
+			$options["p"] = $options["id"];
+			unset($options["id"]);
+		}
+	
+		parent::prepareOptions($options);
+	}
 }
