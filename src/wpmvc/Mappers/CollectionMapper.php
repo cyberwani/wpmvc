02b4aa1903ad47fd0a618f24772b32781f96b005
Jen Wachter <jwachter@jhu.edu>
2013-06-25 15:22:22 -0400

    Created a version of addDataToResult() in the collection mapper. This function creates a new mapper based on the object being analyzed and uses that mapper's addDataToResult() to add the data.

--- src/wpmvc/Mappers/CollectionMapper.php
+++ src/wpmvc/Mappers/CollectionMapper.php
@@ -2,7 +2,17 @@
 
 namespace wpmvc\Mappers;
 
+use \wpmvc\Helpers\ClassFinder;
+
 class CollectionMapper extends Base
 {
-	
+	protected function addDataToResult($item)
+	{
+		$mapper = ClassFinder::find("Mappers", ucfirst($item->post_type) . "Mapper");
+		$mapper = new $mapper(array());
+
+		$item = $mapper->addDataToResult($item);
+
+		return $item;
+	}
 }
