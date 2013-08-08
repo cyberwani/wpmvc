21f1cd9f98ee19461ddcc3c82bde58bf538a52d5
Jen Wachter <jwachter@jhu.edu>
2013-08-08 16:00:31 -0400

    Removed getFeaturedImage() (for now) -- not all post types have this enabled.

--- src/wpmvc/Mappers/PostMapper.php
+++ src/wpmvc/Mappers/PostMapper.php
@@ -23,7 +23,7 @@ class PostMapper extends Base
 
 		$item->url = get_permalink($item->ID);
 		$item->attachments = $this->getAttachments($item);
-		$item->featured_image = $this->getFeaturedImage($item);
+		// $item->featured_image = $this->getFeaturedImage($item);
 		$item->meta = get_post_meta($item->ID);
 
 		return $item;
